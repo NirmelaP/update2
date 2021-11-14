5655132a9c375e2e95551722b216ff8da03b7f76
NirmelaP <94306942+NirmelaP@users.noreply.github.com>
2021-11-14 17:44:37 +0100

    Test1

--- javaScript.js
+++ javaScript.js
@@ -17,23 +17,38 @@ for(i = 0; i< studenter.length; i++){
 document.getElementById("output").innerHTML = output;
 }
 
+
+//Här skapar jag ny funktion addName som har uppgift att lägga till namn.
 function addName(){
+   
+    //Här hämtar man information som användaren skriver in. 
     var namn = document.getElementById("name").value;
+    //Här använder jag if condition som gör att om input är lika med 0 dvs om man har skrivit 
+    //något så körs denna.
     if(namn.length !=0){
+    //Här läggs det till värde från användarens i slutet av en aray.
     studenter[studenter.length] = namn;
     document.getElementById("name").value = "";
+    //Här uppdateras aray med nytt namn.
     uppdateraOutput();
     } 
 }
 
+//Här skapar jag funktion som tar bort namn med perametr "id"".
 function taBort(id){
+    //console-log inbyggd funktion förr att testa om funktion funkar
     console.log("Ta bort: " + id);
+    //skapar en tom temperör variabel
     var studenterTemp = [];
+    //för-loop metod att snurra genom array
     for(i = 0; i< studenter.length; i++){
+        //om id innte sämmer används push metod att radera namn
         if(i != id){
+            //sparar nya array i den temperöra arrayen
             studenterTemp.push(studenter[i]);
         }
     }
+    //updater array
     studenter = studenterTemp;
     uppdateraOutput();
 
